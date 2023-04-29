cd72aa8299aa1a1f8c0177f1a57345afc88dd3d8
779691662@qq.com <779691662@qq.com>
2023-04-29 22:30:21 +0800

    GPU版本球速预测完成

--- Core/src/ssl/gpuBestAlgThread.cpp
+++ Core/src/ssl/gpuBestAlgThread.cpp
@@ -310,7 +310,7 @@ void CGPUBestAlgThread::predictBallPos() {
 		memcpy(_ball_pos_prediction_results, results, output_dim * sizeof(float));
 		free(results);
 		for (int i = 0; i < 3; i++) {
-			std::cout << _ball_pos_prediction_results[i] << " ";
+			std::cout << _ball_pos_prediction_results[i*10] << " ";
 		}
 		std::cout << std::endl;
 		_ball_pos_prediction_mutex->unlock();
