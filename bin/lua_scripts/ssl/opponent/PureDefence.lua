b18e777d9fcc156773632e650e7f4d90e9bf06d5
SYLG <2330180633@qq.com>
2023-05-18 19:52:31 +0800

    pure defence

---
+++ bin/lua_scripts/ssl/opponent/PureDefence.lua
@@ -0,0 +1,29 @@
+gOppoConfig = {
+  ----------------------play---------------------------------------------------------------
+
+	CornerKick  = {8}, -- kf
+	
+	FrontKick   = {61},
+
+	BackKick    = {"Ref_ImmortalKickV61"}, --{10, "Ref_ImmortalKickV1"}
+-------------------------------Def---------------------------------------------------
+
+	CornerDef   = "Ref_CornerDefV2", -- 可能要用v4，防头球
+	BackDef 	= "Ref_BackDefV2",
+	MiddleDef   = "Ref_MiddleDefV2",--横向marktouch用v5，四车markfront用v10,保守打法用V2
+	FrontDef    = "Ref_FrontDefV2", --四车markfront用v9,若挑就用V7，若他们四车全上就用V11,保守打法用Ref_FrontDefV2
+	PenaltyKick = "Ref_PenaltyKickV6", --Ref_PenaltyKick2017V5
+	PenaltyDef  = "Ref_PenaltyDefV6", --Ref_PenaltyDefV3
+	KickOff		= "Ref_KickOffV23",
+	KickOffDef  = "Ref_KickOffDefV23",
+	NorPlay     = "Test_play6"
+}
+
+
+--     B        -250           M        0   F_A  100  F_B  200  F_C   350
+-----------------|----------------------|--------|---------|-----------|---------
+
+
+
+--                                      0    F_D   125    F_E
+----------------------------------------|-----------|----------------------------
