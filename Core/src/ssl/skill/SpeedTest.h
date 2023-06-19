d8bfaeb3842aba39d20394bf446e1f9a6d8f494c
dyingfire <th3058786500@sjtu.edu.cn>
2023-06-19 16:56:05 +0800

    finish speed_test with a small bug: one record missing for the first time to record

--- Core/src/ssl/skill/SpeedTest.h
+++ Core/src/ssl/skill/SpeedTest.h
@@ -1,7 +1,9 @@
 #ifndef __SPEED_TEST_H__
 #define __SPEED_TEST_H__
 #include "PlayerTask.h"
+#include "BufferCounter.h"
 #include "param.h"
+#include <QFile>
 
 class CSpeedTest :public CStatedTask {
 public:
@@ -12,6 +14,20 @@ public:
 protected:
 	virtual void toStream(std::ostream& os) const { os << "Skill: CSpeedTest\n"; }
 private:
+	enum State {
+		STATE_INIT,
+		STATE_GET_READY,
+		STATE_GOTO_1,
+		STATE_GOTO_2,
+		STATE_STOP,
+		STATE_FAIL
 	};
+	QFile *_file1, *_file2;
+	QTextStream *_out1, *_out2;
+	State _state;
+	CBufferCounter statusCounter;
+	int velocityCounter;
+	double _v;
+};
 
 #endif //__SPEED_TEST_H__
