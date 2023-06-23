82be70a9f48fccea4b618f54b4bb48bbdd3b97a4
dyingfire <th3058786500@sjtu.edu.cn>
2023-06-23 17:58:14 +0800

    fix some bugs

--- Core/src/ssl/skill/ProtectBall.cpp
+++ Core/src/ssl/skill/ProtectBall.cpp
@@ -44,7 +44,7 @@ void CProtectBall::plan(const CVisionModule* pVision)
     const BallVisionT& ball = pVision->Ball();
 
     // 预测系数a
-    const double a=4;// 需要根据实际情况调整
+    const double a=3;// 需要根据实际情况调整
     // 计算预测时间
     double ballSpeed = ball.Vel().mod(); // 球的速度
     double predictTime =ballSpeed/a;
@@ -62,51 +62,63 @@ void CProtectBall::plan(const CVisionModule* pVision)
     double self2OppoDist = (pVision->TheirPlayer(theirBestPlayer).Pos() - self.Pos()).mod();
     double oppo2BallDir = Utils::Normalize((ball.Pos() - pVision->TheirPlayer(theirBestPlayer).Pos()).dir());
     CVector oppo2Ball = ball.Pos() - pVision->TheirPlayer(theirBestPlayer).Pos();
-    CGeoPoint defencePos = CGeoPoint(0, 0);
+    CVector advancer2oppo= pVision->TheirPlayer(theirBestPlayer).Pos() - pVision->OurPlayer(advancer).Pos();
+
  
     int new_state = state(), old_state = state();
     do {
         old_state = new_state;
 
-        if (self2predictDist > 100)
-            new_state = Approach_Ball;
-        else if (oppo2BallDist < 20)
-            new_state = Defence;
-        else 
-            new_state = Protect_Ball;
-      
-        //break;
-
         switch (old_state){
         case BEGINNING:
             {
-                if(self2predictDist > 100){
-                    if(verBos) cout<<"BEGINNING-->Approach_Ball"<<endl;
-                    new_state = Approach_Ball;
-                }
-                else{
-                    if(verBos) cout<<"BEGINNING-->Protect_Ball"<<endl;
-                    new_state = Protect_Ball;
-                }
-                break;
+            if (oppo2BallDist < 20) {
+                if (verBos) cout << "BEGINNING-->Defence" << endl;
+                new_state = Defence;
+            }
+            else if (self2predictDist > 100) {
+                if (verBos) cout << "BEGINNING-->Approach_Ball" << endl;
+                new_state = Approach_Ball;
+            }
+            else if (oppo2BallDist > 20 && self2predictDist <= 100) {
+                if (verBos) cout << "BEGINNING-->Protect_Ball" << endl;
+                new_state = Protect_Ball;
+            }
+            else {
+                new_state = BEGINNING;
+            }
+            break;
             }
         case Approach_Ball:
-           if(oppo2BallDist<10&&advancer2BallDist>oppo2BallDist){
-                if(verBos) cout<<"Approach_Ball-->Defence"<<endl;
+            if (oppo2BallDist < 20) {
+                if (verBos) cout << "Approach_Ball-->Defence" << endl;
                 new_state = Defence;
-           }
-           break;
+            }
+            else {
+                new_state = Approach_Ball;
+            }
+            break;
         case Protect_Ball:
-            if(oppo2BallDist<10 ){
-                if(verBos) cout<<"Protect_Ball-->Defence"<<endl;
+            if (oppo2BallDist < 20) {
+                if (verBos) cout << "Protect_Ball-->Defence" << endl;
                 new_state = Defence;
+            }
+            else if (self2predictDist > 100) {
+                if (verBos) cout << "Protect_Ball-->Approach_Ball" << endl;
+                new_state = Approach_Ball;
+            }else {
+                new_state = Protect_Ball;
             }
             break;
         case Defence:
-            if (oppo2BallDist > 50) {
-				if(verBos) cout<<"Defence-->Protect_Ball"<<endl;
-				new_state = Protect_Ball;
-			}
+            if (oppo2BallDist > 20) {
+                if (verBos) cout << "Defence-->Protect_Ball" << endl;
+                new_state = Protect_Ball;
+            }
+            else {
+                new_state = Defence;
+            }
+            break;
         default:
             new_state = BEGINNING;
             break;
@@ -128,7 +140,7 @@ void CProtectBall::plan(const CVisionModule* pVision)
     switch (state()){
     case Approach_Ball:
         {
-            CGeoPoint approachBallPos = predictBallPos + Utils::Polar2Vector(oppo2BallDist/2, Utils::Normalize((pVision->TheirPlayer(theirBestPlayer).Pos() - ball.Pos()).dir()));
+            CGeoPoint approachBallPos = predictBallPos+ Utils::Polar2Vector(50, Utils::Normalize((predictBallPos-self.Pos()).dir())+Param::Math::PI);
             double approachBallDir = (approachBallPos - self.Pos()).dir();
             protectTask.player.pos=approachBallPos;
             protectTask.player.angle=approachBallDir;
@@ -144,15 +156,14 @@ void CProtectBall::plan(const CVisionModule* pVision)
  
     case Protect_Ball:
         {
-            static CGeoPoint lastProtectBallPos;
             CGeoPoint protectBallPos;
-            protectBallPos = pVision->TheirPlayer(theirBestPlayer).Pos() + Utils::Polar2Vector(2*Param::Vehicle::V2::PLAYER_FRONT_TO_CENTER, Utils::Normalize((ball.Pos()-pVision->TheirPlayer(theirBestPlayer).Pos()).dir()));
-            double protectBallDir = Utils::Normalize((predictBallPos - pVision->TheirPlayer(theirBestPlayer).Pos()).dir());
-            protectTask.player.pos=lastProtectBallPos;
+            protectBallPos = predictBallPos + Utils::Polar2Vector(advancer2oppo.mod() / 2, Utils::Normalize(advancer2oppo.dir()));
+            double protectBallDir = (Utils::Normalize((predictBallPos - pVision->TheirPlayer(theirBestPlayer).Pos()).dir()) + Utils::Normalize((predictBallPos - self.Pos()).dir())) / 2;
+            protectTask.player.pos= protectBallPos;
             protectTask.player.angle=protectBallDir;
             protectTask.player.vel=CVector(0,0);
             protectTask.player.rotvel=0;
-            protectTask.player.max_acceleration=500;
+            protectTask.player.max_acceleration=1000;
             protectTask.player.max_deceleration=1000;
             setSubTask(TaskFactoryV2::Instance()->SmartGotoPosition(protectTask));
             GDebugEngine::Instance()->gui_debug_msg(CGeoPoint(0,200), "Protect Ball");
@@ -160,14 +171,16 @@ void CProtectBall::plan(const CVisionModule* pVision)
         break;
     case Defence:
         {
+            static CGeoPoint lastDefencePos;
+            CGeoPoint defencePos = CGeoPoint(0, 0);
             if (oppo2BallDir > -Param::Math::PI * 2 / 3 && oppo2BallDir < Param::Math::PI * 2 / 3) {
-                defencePos = predictBallPos + Utils::Polar2Vector(100, Utils::Normalize((ball.Pos() - CGeoPoint(-Param::Field::PITCH_LENGTH / 2, 0)).dir() + Param::Math::PI));
+                defencePos = predictBallPos + Utils::Polar2Vector(75, Utils::Normalize((ball.Pos() - CGeoPoint(-Param::Field::PITCH_LENGTH / 2, 0)).dir() + Param::Math::PI));
             }
             else if (oppo2BallDir <= -Param::Math::PI * 2 / 3) {
-                defencePos = predictBallPos + Utils::Polar2Vector(100, Utils::Normalize(oppo2Ball.dir()) - (Param::Math::PI - fabs(oppo2BallDir)) * (Param::Math::PI - fabs(oppo2BallDir))/Param::Math::PI);//方向修正
+                defencePos = predictBallPos + Utils::Polar2Vector(75, Utils::Normalize(oppo2Ball.dir()) - (Param::Math::PI - fabs(oppo2BallDir)) * (Param::Math::PI - fabs(oppo2BallDir))/Param::Math::PI);//方向修正
             }
             else if (oppo2BallDir >= Param::Math::PI * 2 / 3) {
-                defencePos = predictBallPos + Utils::Polar2Vector(100, Utils::Normalize(oppo2Ball.dir()) + (Param::Math::PI - fabs(oppo2BallDir)) * (Param::Math::PI - fabs(oppo2BallDir)) / Param::Math::PI);//方向修正
+                defencePos = predictBallPos + Utils::Polar2Vector(75, Utils::Normalize(oppo2Ball.dir()) + (Param::Math::PI - fabs(oppo2BallDir)) * (Param::Math::PI - fabs(oppo2BallDir)) / Param::Math::PI);//方向修正
             }
 			double defenceDir = (predictBallPos -self.Pos()).dir();
 			protectTask.player.pos=defencePos;
