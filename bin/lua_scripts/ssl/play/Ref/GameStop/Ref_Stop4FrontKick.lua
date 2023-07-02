6c0c38fa7d48073d3ca989027d400b13b5f34338
dyingfire <th3058786500@sjtu.edu.cn>
2023-07-02 16:46:54 +0800

    CornerKick & FrontKick

--- bin/lua_scripts/ssl/play/Ref/GameStop/Ref_Stop4FrontKick.lua
+++ bin/lua_scripts/ssl/play/Ref/GameStop/Ref_Stop4FrontKick.lua
@@ -4,8 +4,8 @@
 
 local STOP_FLAG  = bit:_or(flag.slowly, flag.dodge_ball)
 local STOP_DSS   = bit:_or(flag.dodge_ball, flag.allow_dss)
-local KICK_POS   = function ()
-  return ball.syntYPos(CGeoPoint:new_local(ball.posX() - 59, param.pitchWidth/2-18))()
+local WAIT_BALL_POS   = function ()
+  return ball.pos() + Utils.Polar2Vector(50, ball.syntY(0.5 * math.pi))
 end
 
 local KICK_DIR  = ball.antiYDir(1.57)
@@ -24,7 +24,7 @@ firstState = "start",
       return "exit"
     end   
   end,
-  Assister = task.goCmuRush(KICK_POS, KICK_DIR, ACC, STOP_DSS),
+  Assister = task.goCmuRush(WAIT_BALL_POS, KICK_DIR, ACC, STOP_DSS),
   Leader   = task.goCmuRush(FRONT_POS1, _, ACC, STOP_DSS),
   Middle   = task.goCmuRush(FRONT_POS2, _, ACC, STOP_DSS),
   Special  = task.goCmuRush(FRONT_POS3, _, ACC, STOP_DSS),
