158db6780e65ce16ec84c95a0e501ec08ab67ace
葛毅恒 <geyiheng@sjtu.edu.cn>
2024-02-11 17:35:24 +0800

    added backpos function to calculate the virtual point for a robot in order to intercept a ball faster than normal

--- Core/src/ssl/skill/TechDefenceRobot.cpp
+++ Core/src/ssl/skill/TechDefenceRobot.cpp
@@ -36,7 +36,6 @@ public:
     Intercept(const CGeoPoint& O, const double& DIR, const CGeoPoint& A) 
         : O(O), DIR(DIR), A(A), M(O, CGeoPoint(O.x() + std::cos(DIR), O.y() + std::sin(DIR))) {
         H = M.projection(A); // 计算垂足H并存储
-        AHdist = A.dist(H); // 计算A到H的距离并存储
     }
     
     CGeoPoint FootH() const {
@@ -48,7 +47,10 @@ public:
     }
 
     double DistanceAH() const {
-        return AHdist; // 直接返回A到H的距离
+        return A.dist(H); // 直接返回A到H的距离
+    }
+    double DistanceOH() const {
+    	return O.dist(H);
     }
 
 private:
@@ -59,7 +61,24 @@ private:
     CGeoPoint H;
     double AHdist;
 };
-
+double timeOH(double v0, double s) {
+    // a = -std::abs(a);
+    double a=-1.93;
+    double t = (-v0 + std::sqrt(v0 * v0 - 2 * a * (-s))) / a;
+    
+    return t;
+}
+double distback(const double& SAH, const double& TAH) {
+	double maxa=4;
+    double SF = 0.5 * maxa * std::pow(TAH, 2); // 使用std::pow进行幂运算
+    if (SAH >= SF) {
+        return 2 * SF; // 假设这里是期望的返回逻辑
+    } else if (0.5 * SF < SAH && SAH < SF) { // 分开进行比较
+        double T2 = TAH * std::pow((SF - SAH) / (2 * SF), 0.5); // 再次使用std::pow
+        return 0.5 * maxa * std::pow(2 * (TAH - T2), 2); // 修改括号和幂运算
+    }
+    return 0; // 如果上述条件都不满足，应该有一个默认返回值
+}
 
 
 CTechDefence::CTechDefence() 
@@ -73,7 +92,7 @@ void CTechDefence::plan(const CVisionModule* pVision)
 	int rolenum=task().executor;
 	taskR1.executor=rolenum;
 	//set the executor of this plan
-//----------------------------------------------
+//----------------------------------------------INITIALIZE ALL INFOS
 	std::vector<const PlayerVisionT*> OPptrs;
 	for (int irole = 1; irole <= Param::Field::MAX_PLAYER; ++irole)
 	{
@@ -83,26 +102,28 @@ void CTechDefence::plan(const CVisionModule* pVision)
 			OPptrs.push_back(&OPtmp);
 		}
 	}
-	const BallVisionT& ball = pVision->Ball();//ball.Pos().X()和ball().Pos().Y()  ball.Vel().mod() 
-// ---------------------------------------------
-	CGeoPoint O(0, 0); // 射线的起点
-    double DIR = M_PI / 4; // 射线的方向，以弧度表示，这里是45度角
-    CGeoPoint A(3, 1); // 射线外的点A
-
+	const BallVisionT& ball = pVision->Ball();//ball.Pos().x() ball().Pos().y()  ball.Vel().mod() ball.Vel().dir()
+// ---------------------------------------------PREPARE INPUTS FOR CALSS INTERCEPT 
+	CGeoPoint O;
+	O=ball.Pos(); // 射线的起点
+    double DIR =ball.Vel().dir(); 
+    CGeoPoint A(3, 1); // 射线外的点A,应该先有一个谁快要接到球的判断车号，或踢球者也可以改这个谁快要接到球的车号
+// ----------------------------------------------GET INFOS FROM CLASS INTERCEPT
     Intercept calculator(O, DIR, A);
     CGeoPoint H = calculator.FootH(); // 获取垂足H的坐标
     CGeoLine M = calculator.LineM(); // 获取射线M
-    double distance = calculator.DistanceAH(); // 获取A到H的距离
-    std::cout << "FootH: (" << H.x() <<","<<H.y()<<")";
-    // std::cout << "LineM: " << M << std::endl;
-    std::cout << "DistanceAH: " << distance ;
-//----------------------------------------------
+    double SAH = calculator.DistanceAH(); // 获取A到H的距离
+    std::cout << "FootH: (" << H.x() <<","<<H.y()<<")";  
+//----------------------------------------------PREPARE INFOS FOR VIRTUAL TARGET POS TO MAKE INTERCEPTION FASTER
+    double TAH=timeOH(ball.Vel().mod(),SAH);
+    std::cout << "DistanceAH: " << SAH ;
+//----------------------------------------------CALCULATE DISTBACK AND POSBACK TO MAKE INTERCEPTION FASTER
+    double backdist=distback(SAH,TAH);
+    std::cout <<"backdist<"<<backdist<<">";
+//----------------------------------------------SET SUB TASKS
 	setSubTask(TaskFactoryV2::Instance()->GotoPosition(taskR1));//将taskR1给走位subtask执行
 	CStatedTask::plan(pVision);
 }
-
-
-
 
 
 CTechDefence::~CTechDefence() {
