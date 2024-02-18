5a2b053fa3d167462275dc2819569284017e9b59
葛毅恒 <geyiheng@sjtu.edu.cn>
2024-02-18 14:10:28 +0800

    adjustable jam radius

--- Core/src/ssl/skill/TechDefenceRobot.cpp
+++ Core/src/ssl/skill/TechDefenceRobot.cpp
@@ -5,7 +5,7 @@
 
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
-
+#endif
 
 #include <iostream>
 #include "GDebugEngine.h"
@@ -25,6 +25,7 @@
 using namespace std;
 
 namespace {enum TDstate {getball ,wait};}
+int rJam=45;
 int findIndex(const std::vector<int>& vec, int target) {
     auto it = std::find(vec.begin(), vec.end(), target);
     if (it != vec.end()) {return std::distance(vec.begin(), it);}
@@ -82,6 +83,16 @@ double gotoAngle(double a,double b,double c,double d,double DIR,double VR,CGeoPo
     else if (a_c)                 {drawDir(A,DIR,to_string(a_c).c_str());return a_c;}
     else if (amidDir(a,b,DIR,1,0)){drawDir(A,DIR,"a b");return a;}
     else if (amidDir(b,c,DIR,1,0)){drawDir(A,DIR,"b c");return c;}}
+CGeoPoint JamA2Jampos(CGeoPoint Apos,double DIR){   
+    vector<CGeoPoint>  centers  = { CGeoPoint(75,-130), CGeoPoint(75,130), CGeoPoint(-150,0) };
+    vector<CGeoCirlce> Circles  = { CGeoCirlce(centers[0], rJam), CGeoCirlce(centers[1], rJam), CGeoCirlce(centers[2], rJam) };
+    int icircle;
+    for(int i = 0; i < 3; ++i){if (centers[i].dist(Apos)<32){icircle=i;break;}}
+    CGeoPoint JamP(Apos.x() + 200 * std::cos(DIR), Apos.y() + 200 * std::sin(DIR));
+    CGeoSegment JamPline(JamP,Apos);
+    GDebugEngine::Instance()->gui_debug_line(JamP,Apos,COLOR_CYAN);
+    CGeoSegmentCircleIntersection Intersection = CGeoSegmentCircleIntersection(JamPline,Circles[icircle]);
+    return Intersection.point1();}
 CGeoPoint midpoint1(const CGeoPoint& A, const CGeoPoint& B) {return CGeoPoint((A.x() + B.x()) / 2.0, (A.y() + B.y()) / 2.0);}
 CTechDefence::CTechDefence(){}
 
@@ -127,22 +138,24 @@ void CTechDefence::plan(const CVisionModule* pVision){
     // std::cout<<"findIndex(TProlenums,TProlenumsR[1])="<<findIndex(TProlenums,TProlenumsR[1])<<std::endl;
     // std::cout<<"TPptrs[1] pos x="<<TPptrs[TProlenumsR[0]]->Pos().x()<<std::endl;
     std::cout<<"b:{"<<b<<"};d{"<<d<<"};a{"<<a<<"};c{"<<c<<"}"<<std::endl;
+    CGeoPoint C01(75, -130);CGeoPoint C02(75, 130);CGeoPoint C03(-150, 0);
+    GDebugEngine::Instance()->gui_debug_arc(C01, 30, 0, 360, COLOR_YELLOW);
+    GDebugEngine::Instance()->gui_debug_arc(C02, 30, 0, 360, COLOR_YELLOW);
+    GDebugEngine::Instance()->gui_debug_arc(C03, 30, 0, 360, COLOR_YELLOW);
     drawDir(A,a,"a");drawDir(A,b,"b");drawDir(A,c,"c");drawDir(A,d,"d");
 //-------------------------------------------------GO TO WHICH ANGLE
     double JamAngle=gotoAngle(a,b,c,d,DIR,VR,A);
     drawDir(A,JamAngle,"JamAngle");
-    CGeoPoint Jam(A.x() + 40 * std::cos(JamAngle), A.y() + 40 * std::sin(JamAngle));
+    CGeoPoint Jampos=JamA2Jampos(A,JamAngle);
 //-------------------------------------------------SET TASK 
     TaskT taskR1(task());
     int rolenum=task().executor;
     taskR1.executor=OProlenums[0];
-    taskR1.player.pos=Jam;
+    taskR1.player.pos=Jampos;
     taskR1.player.angle=normalizeAngle1(DIR-M_PI);
 	setSubTask(TaskFactoryV2::Instance()->GotoPosition(taskR1));//将taskR1给走位subtask执行
 	CStatedTask::plan(pVision);}
-
 CTechDefence::~CTechDefence() {}
 
 CPlayerCommand* CTechDefence::execute(const CVisionModule* pVision)
 {if (subTask()){return subTask()->execute(pVision);} return NULL;}
-#endif
